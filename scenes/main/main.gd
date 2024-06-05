14ed5c9898292da2ac07098053c82dd8c1beb653
Nael <willowitzy@gmail.com>
2024-06-05 10:07:39 -0300

    Input Map & Global Script

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@export var next_scene = PackedScene
 @onready var scene = get_tree()
 
 func _ready():
@@ -10,4 +10,5 @@ func _on_quit_button_pressed():
 	scene.quit()
 
 func _on_play_button_pressed():
-	scene.change_scene_to_file("res://scenes/world/world.tscn")
+	scene.change_scene_to_packed(next_scene)
+	
